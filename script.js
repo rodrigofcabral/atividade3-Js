4ab03b2e4befc6b38d0e9766d5c0bcf9ef230419
Rodrigo Cabral <rodrigocabral1302@gmail.com>
2024-06-01 01:41:11 -0300

    subindo projeto no github

---
+++ script.js
@@ -0,0 +1,16 @@
+// Exercício //
+
+var idade = prompt("Digite a idade:")
+
+if (idade >= 0 && idade < 15) {
+    document.write("Criança");
+} else if (idade >= 15 && idade < 30) {
+    document.write("Jovem");
+} else if (idade >= 30 && idade < 60) {
+    document.write("Adulto");
+} else if (idade >= 60) {
+    document.write("Idoso");
+} else {
+    document.write("Informe uma idade válida!")
+}
+
